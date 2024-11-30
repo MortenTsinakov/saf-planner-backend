43a2b20a632e1bcd98e2021e0214a9c0bc7fc644
Morten Tšinakov <morten.tshinakov@gmail.com>
2024-11-30 15:24:30 +0200

    Base for SAF server

---
+++ src/main/java/hr/adriaticanimation/saf_planner/SafPlannerApplication.java
@@ -0,0 +1,13 @@
+package hr.adriaticanimation.saf_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/adriaticanimation/saf_planner/SafPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.adriaticanimation.saf_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
