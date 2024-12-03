3fef6e1621744b76149370efc8a906887bd6ebbe
Morten Tšinakov <morten.tshinakov@gmail.com>
2024-12-03 17:30:33 +0200

    Add CORS to FilterChain

--- src/main/java/hr/adriaticanimation/saf_planner/configuration/SecurityConfiguration.java
+++ src/main/java/hr/adriaticanimation/saf_planner/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package hr.adriaticanimation.saf_planner.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auths -> auths
                         .requestMatchers(SecurityConstants.PUBLIC_URLS).permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
