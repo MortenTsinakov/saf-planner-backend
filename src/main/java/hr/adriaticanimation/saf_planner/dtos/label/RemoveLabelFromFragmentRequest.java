c9178519ca330de53f47bbfb2b2d36c753c0ceab
Morten Tšinakov <morten.tshinakov@gmail.com>
2025-01-30 14:09:12 +0200

    Add validation annotations for requests

--- src/main/java/hr/adriaticanimation/saf_planner/dtos/label/RemoveLabelFromFragmentRequest.java
+++ src/main/java/hr/adriaticanimation/saf_planner/dtos/label/RemoveLabelFromFragmentRequest.java
@@ -1,7 +1,11 @@
 package hr.adriaticanimation.saf_planner.dtos.label;
 
+import jakarta.validation.constraints.NotNull;
+
 public record RemoveLabelFromFragmentRequest(
+        @NotNull
         Long labelId,
+        @NotNull
         Long fragmentId
 ) {
 }
