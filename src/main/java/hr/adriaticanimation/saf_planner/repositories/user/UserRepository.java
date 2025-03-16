4aa0d06005d3d96093aecfceb08d7c781e00834f
Morten Tšinakov <morten.tshinakov@gmail.com>
2025-03-16 18:12:47 +0200

    Implement user search by full name

--- src/main/java/hr/adriaticanimation/saf_planner/repositories/user/UserRepository.java
+++ src/main/java/hr/adriaticanimation/saf_planner/repositories/user/UserRepository.java
@@ -2,9 +2,18 @@ package hr.adriaticanimation.saf_planner.repositories.user;
 
 import hr.adriaticanimation.saf_planner.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    @Query(value = "SELECT * FROM users " +
+           "WHERE similarity(first_name, :searchTerm) > :threshold " +
+           "OR similarity(last_name, :searchTerm) > :threshold " +
+           "ORDER BY (similarity(first_name, :searchTerm) + similarity(last_name, :searchTerm)) DESC " +
+           "LIMIT 20", nativeQuery = true)
+    List<User> findByFullName(@Param("searchTerm") String searchTerm, @Param("threshold") double threshold);
 }
